a457ee04f1cfcfef2ef8b997e8727f43bc2f941b
jawello <jawello@gmail.com>
2019-11-11 23:08:12 +0300

    Make full db generation

--- e-shop_api/data_faker/generate_data.py
+++ e-shop_api/data_faker/generate_data.py
@@ -3,11 +3,15 @@ from app.models import Product
 from app.models import Shop
 from app.models import ProductShop
 from app.models import Basket
+from app.models import ProductInBasket
+from app.models import Order
 
 from faker import Faker
 
 from app.db import construct_db_url
 from app.settings import load_config
+from app.security import generate_password_hash
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -24,7 +28,7 @@ def generate_users(session: Session, count: int) -> list:
     result = []
     for i in range(count):
         fake = faker.simple_profile(sex=None)
-        user = Users(name=fake['name'], login=fake['username'], password=default_pass)
+        user = Users(name=fake['name'], login=fake['username'], password=generate_password_hash(default_pass))
         result.append(user)
         log.info(f"Generate user: {user}")
         session.add(user)
@@ -99,8 +103,57 @@ def generate_basket(session: Session, count: int, users: list) -> list:
     session.commit()
     for i in result:
         session.refresh(i)
-        log.info(f"Generate product_shop: {i}")
+        log.info(f"Generate basket: {i}")
     return result
+
+
+def generate_product_in_basket(session: Session, baskets: list, products_shops: list):
+    result = []
+    for basket in baskets:
+        products_in_basket_count = random.randint(1, 20)
+        product_was = set()
+        for i in range(products_in_basket_count):
+            product_shop = products_shops[random.randint(0, len(products_shops) - 1)]
+            while product_shop.id in product_was:
+                product_shop = products_shops[random.randint(0, len(products_shops) - 1)]
+
+            product_was.add(product_shop.id)
+            if product_shop.quantity == 1:
+                quantity = 1
+            else:
+                quantity = random.randint(1, product_shop.quantity)
+            product_in_basket = ProductInBasket(basket=basket,
+                                                product_shop=product_shop,
+                                                quantity=quantity
+                                                )
+            session.add(product_in_basket)
+            result.append(product_in_basket)
+
+    session.commit()
+    for i in result:
+        session.refresh(i)
+        log.info(f"Generate product_in_basket: {i}")
+
+
+def generate_order(session: Session, count: int, baskets: list):
+    result = []
+    statuses = ["check availability", "awaiting payment", "paid"]
+    faker = Faker()
+    basket_was = set()
+    for i in range(count):
+        while True:
+            basket = baskets[random.randint(0, len(baskets) - 1)]
+            if basket not in basket_was:
+                basket_was.add(basket)
+                break
+        status = statuses[random.randint(0, len(statuses) - 1)]
+        order = Order(basket=basket, status=status, date=faker.date_time_this_year())
+        session.add(order)
+        result.append(order)
+    session.commit()
+    for i in result:
+        session.refresh(i)
+        log.info(f"Generate order: {i}")
 
 
 def main(config_path):
@@ -116,6 +169,8 @@ def main(config_path):
     shops = generate_shop(session, 10)
     products_shops = generate_product_shop(session, 100, products, shops)
     baskets = generate_basket(session, 100, users)
+    generate_product_in_basket(session, baskets, products_shops)
+    generate_order(session, 50, baskets)
 
 
 if __name__ == '__main__':
@@ -129,4 +184,3 @@ if __name__ == '__main__':
         main(args.config)
     else:
         parser.print_help()
-
