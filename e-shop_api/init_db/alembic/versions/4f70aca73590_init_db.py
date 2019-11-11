70b4d607d5336c4d214ae2b5d1b8a56c098d89de
jawello <jawello@gmail.com>
2019-11-11 04:29:53 +0300

    Fix migration file

--- e-shop_api/init_db/alembic/versions/4f70aca73590_init_db.py
+++ e-shop_api/init_db/alembic/versions/4f70aca73590_init_db.py
@@ -16,13 +16,12 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table('uua',
+    op.create_table('users',
                     sa.Column('id', sa.Integer, primary_key=True),
                     sa.Column('name', sa.String, nullable=False),
-                    sa.Column('login', sa.String, nullable=False),
+                    sa.Column('login', sa.String, nullable=False, unique=True),
                     sa.Column('password', sa.String, nullable=False)
                     )
-    op.create_unique_constraint('uq_uua_login', 'uua', ['login'])
 
     op.create_table('product',
                     sa.Column('id', sa.Integer, primary_key=True),
@@ -31,7 +30,7 @@ def upgrade():
 
     op.create_table('shop',
                     sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String, nullable=False),
+                    sa.Column('name', sa.String, nullable=False, unique=True),
                     sa.Column('description', sa.String),
                     sa.Column('site', sa.String))
 
@@ -47,22 +46,21 @@ def upgrade():
 
     op.create_table('basket',
                     sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('user_id', sa.Integer, nullable=False),
+                    sa.Column('users_id', sa.Integer, sa.ForeignKey('users.id'), nullable=False)
                     )
 
     op.create_table('product_in_basket',
                     sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('basket_id', sa.Integer, sa.ForeignKey('basket.id'), nullable=False),
-                    sa.Column('product_shop_ids', sa.Integer, nullable=True),
+                    sa.Column('basket_id', sa.Integer, sa.ForeignKey('basket.id'), nullable=False, unique=True),
+                    sa.Column('product_shop_id', sa.Integer, sa.ForeignKey('product_shop.id'), nullable=True),
                     sa.Column('quantity', sa.Integer, default=1)
                     )
-    op.create_unique_constraint('uq_product_basket_id', 'product_in_basket', ['basket_id'])
     op.create_check_constraint('ck_quantity_basket', 'product_in_basket', 'quantity > 0')
 
-    order_status_table = op.create_table('order_status',
-                                         sa.Column('id', sa.String, primary_key=True))
+    order_status = op.create_table('order_status',
+                                   sa.Column('id', sa.String, primary_key=True))
     op.bulk_insert(
-        order_status_table,
+        order_status,
         [
             {"id": "check availability"},
             {"id": "awaiting payment"},
@@ -72,20 +70,17 @@ def upgrade():
 
     op.create_table('order',
                     sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('user_id', sa.Integer, nullable=False),
-                    sa.Column('basket_id', sa.Integer, nullable=False),
+                    sa.Column('users_id', sa.Integer, sa.ForeignKey('users.id'), nullable=False),
+                    sa.Column('basket_id', sa.Integer, nullable=False, unique=True),
                     sa.Column('date', sa.DateTime, nullable=False),
-                    sa.Column('status', sa.String, sa.ForeignKey('order_status.id'), nullable=False),
+                    sa.Column('status', sa.String, sa.ForeignKey('order_status.id'), nullable=False)
                     )
-    op.create_unique_constraint('uq_order_basket_id', 'order', ['basket_id'])
 
 
 def downgrade():
-    op.drop_constraint('uq_order_basket_id', 'order')
     op.drop_table('order')
     op.drop_table('order_status')
 
-    op.drop_constraint('uq_product_basket_id', 'product_in_basket')
     op.drop_constraint('ck_quantity_basket', 'product_in_basket')
     op.drop_table('product_in_basket')
     op.drop_table('basket')
@@ -96,5 +91,6 @@ def downgrade():
     op.drop_table('shop')
     op.drop_table('product')
 
-    op.drop_constraint('uq_uua_login', 'uua')
-    op.drop_table('uua')
+    op.drop_table('users')
+
+
