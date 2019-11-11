ed680a5ce7a685debb4cfa621e955f07c8d98b4a
jawello <jawello@gmail.com>
2019-11-11 23:07:17 +0300

    Remove users fk from Order

--- e-shop_api/app/models/order.py
+++ e-shop_api/app/models/order.py
@@ -7,8 +7,6 @@ from app.models import Base
 class Order(Base):
     __tablename__ = 'order'
     id = sa.Column(sa.Integer, primary_key=True)
-    users_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
-    users = relationship("Users", back_populates="order")
     basket_id = sa.Column(sa.Integer, sa.ForeignKey("basket.id"))
     basket = relationship("Basket", back_populates="order")
     date = sa.Column(sa.DateTime)
@@ -16,14 +14,14 @@ class Order(Base):
     order_status = relationship("OrderStatus", back_populates="order")
 
     def __repr__(self):
-        return "<Order('%s','%s', '%s', '%s', '%s')>" % (self.id, self.user_id, self.basket_id, self.date, self.status)
+        return "<Order('%s','%s', '%s', '%s')>" % (self.id, self.basket_id, self.date, self.status)
 
     @classmethod
     def from_json(cls, data):
         return cls(**data)
 
     def to_json(self):
-        to_serialize = ['basket', 'users', 'date']
+        to_serialize = ['basket', 'date', 'status']
         d = {}
         for attr_name in to_serialize:
             d[attr_name] = getattr(self, attr_name)
