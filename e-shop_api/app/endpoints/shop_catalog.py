8fd1ff27d07e96883a4d012e5475ceff2dd1bd53
jawello <jawello@gmail.com>
2019-11-12 03:58:09 +0300

    Make API shop_catalog

---
+++ e-shop_api/app/endpoints/shop_catalog.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from aiohttp.web import Request
+from aiohttp.web_response import Response
+from aiohttp_rest_api import AioHTTPRestEndpoint
+from aiohttp_rest_api.responses import respond_with_json
+from aiohttp_security import authorized_userid
+
+from app.models import Shop
+
+
+class ShopCatalogEndpoint(AioHTTPRestEndpoint):
+    def connected_routes(self) -> List[str]:
+        return [
+            '/shop_catalog'
+        ]
+
+    @staticmethod
+    async def get(request: Request) -> Response:
+        login = await authorized_userid(request)
+        if not login:
+            return respond_with_json({"error": "Unauthorized"}, status=401)
+
+        shop_id = request.query.get('id')
+
+        if not shop_id:
+            return respond_with_json({"error": "No shop id in request"}, status=400)
+
+        db_pool = request.app['db_pool']
+
+        result = []
+
+        shop = await Shop.get_shop_by_id(db_pool, shop_id)
+        for product_shop in shop.product_shop:
+            result.append({"name": product_shop.product.name, "description": product_shop.product.description,
+                           "price": product_shop.price, "quantity": product_shop.quantity})
+
+        return respond_with_json(result)
+
