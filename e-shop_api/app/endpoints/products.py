503df72b9d9d8153bdb5f55d2993d602a738990f
jawello <jawello@gmail.com>
2020-04-25 12:17:27 +0300

    Make products API

--- e-shop_api/app/endpoints/products.py
+++ e-shop_api/app/endpoints/products.py
@@ -1,4 +1,4 @@
-from aiohttp.web import Request, HTTPInternalServerError
+from aiohttp.web import Request, HTTPInternalServerError, HTTPBadRequest
 from aiohttp.web_response import Response
 from aiohttp import web
 import json
@@ -6,6 +6,7 @@ import json
 from models.product import Product
 from models.schemas.product_schema import ProductSchema
 from models.product_shop import ProductShop
+from models.schemas.product_shop_schema import ProductShopSchema
 
 from sqlalchemy.orm import sessionmaker
 
@@ -29,9 +30,67 @@ async def products_get(request: Request) -> Response:
         else:
             result = session.query(Product).all()
 
-        products_result = ProductSchema(many=True).dump(result)
+        params = request.rel_url.query.get('output')
+        if params:
+            output = [x.strip() for x in params.split(',')]
+            products_serialized = ProductSchema(many=True, only=output).dump(result)
+        else:
+            products_serialized = ProductSchema(many=True).dump(result)
 
-        return Response(body=json.dumps(products_result))
+        return Response(body=json.dumps(products_serialized))
     except Exception as ex:
-        log.warning(f"Endpoint: shop_catalog, Method: get. Error:{str(ex)}")
+        log.warning(f"Endpoint: products, Method: get. Error:{str(ex)}")
         return HTTPInternalServerError()
+
+
+@routes.get("/products/{id}")
+async def products_id_get(request: Request) -> Response:
+    try:
+        conn = request.app['db_pool']
+        session_maker = sessionmaker(bind=conn)
+        session = session_maker()
+
+        product_id = request.match_info['id']
+        if not product_id:
+            return HTTPBadRequest()
+
+        result = session.query(Product).filter_by(id=product_id).first()
+
+        params = request.rel_url.query.get('output')
+        if params:
+            output = [x.strip() for x in params.split(',')]
+            products_serialized = ProductSchema(only=output).dump(result)
+        else:
+            products_serialized = ProductSchema().dump(result)
+
+        return Response(body=json.dumps(products_serialized))
+    except Exception as ex:
+        log.warning(f"Endpoint: products/id, Method: get. Error:{str(ex)}")
+        return HTTPInternalServerError()
+
+
+@routes.post('/products')
+async def products_post(request: Request) -> Response:
+    try:
+        data = await request.json()
+
+        conn = request.app['db_pool']
+        session_maker = sessionmaker(bind=conn)
+        session = session_maker()
+        if data:
+            product_shop_data = data.pop('product_shop', None)
+            product = ProductSchema().load(data, session=session)
+            session.add(product)
+            session.commit()
+
+            product_shop_data['product_id'] = product.id
+            product_shop = ProductShopSchema().load(product_shop_data, session=session)
+            session.add(product_shop)
+            session.commit()
+            return Response(headers={'location': f"/products/{product.id}"})
+        else:
+            return HTTPBadRequest()
+    except Exception as ex:
+        log.warning(f"Endpoint: /products, Method: post. Error:{str(ex)}")
+        return HTTPInternalServerError()
+
--- e-shop_api/app/routes.py
+++ e-shop_api/app/routes.py
@@ -1,7 +1,7 @@
 from endpoints.sessions import sessions_post, sessions_delete
 from endpoints.users import users_get, users_post
 from endpoints.shops import shops_get, shops_id_get, shops_post
-from endpoints.products import products_get
+from endpoints.products import products_get, products_id_get, products_post
 from aiohttp.web import Application
 
 
@@ -18,3 +18,6 @@ def setup_routes(app: Application):
     app.router.add_post('/shops', shops_post, name='shop_create')
 
     app.router.add_get('/products', products_get, name='products_list')
+    app.router.add_get('/products/{id}', products_id_get, name='product_info')
+    app.router.add_post('/products', products_post, name='product_create')
+
